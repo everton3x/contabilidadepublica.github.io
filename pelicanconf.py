2cba322e4f24aa063769aff0f4c91014e1b12db2
Everton da Rosa <everton3x@gmail.com>
2017-06-09 16:54:35 -0300

    Término da migração do blog antigo

--- pelicanconf.py
+++ pelicanconf.py
@@ -27,12 +27,11 @@ AUTHOR_FEED_RSS = None
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         ('Jinja2', 'http://jinja.pocoo.org/'))
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Facebook', 'https://facebook.com/everton3x'),
+          ('Medium', 'https://medium.com/everton3x'),)
 
 DEFAULT_PAGINATION = 10
 
